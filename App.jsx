0a9e9658a82b599f3dded5b376c361f0e4966797
DarkRo <63647187+KurodaTakumi0812@users.noreply.github.com>
2021-02-11 22:42:13 +0900

    Implement signup and login

--- App.jsx
+++ App.jsx
@@ -1,13 +1,62 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
 import MemoEditScreen from './src/screens/MemoEditScreen';
 import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import firebase from 'firebase';
+
+import { firebaseConfig } from './env';
+
+
+const Stack = createStackNavigator();
+
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName='SignUp'
+        screenOptions={{
+          headerStyle: { backgroundColor: '#467FD3' },
+          headerTitleStyle: { color: '#FFFFFF' },
+          headerTitle: 'Memo App',
+          headerTintColor: '#FFFFFF',
+          headerBackTitle: 'Back',
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+        }}
+      >
+        <Stack.Screen name='MemoList' component={MemoListScreen} />
+        <Stack.Screen name='MemoDetail' component={MemoDetailScreen} />
+        <Stack.Screen name='MemoEdit' component={MemoEditScreen} />
+        <Stack.Screen name='MemoCreate' component={MemoCreateScreen} />
+        <Stack.Screen
+          name='LogIn'
+          component={LogInScreen}
+          options={{
+            cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+          }}
+        />
+        <Stack.Screen
+          name='SignUp'
+          component={SignUpScreen}
+          options={{
+            cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/screens/MemoEditScreen.jsx
+++ src/screens/MemoEditScreen.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { View, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native';
 
-import AppBar from '../components/AppBar';
-import CircleButton from '../components/CircleButton'
+import CircleButton from '../components/CircleButton';
+import KeyboardSafeView from '../components/KeyboardSafeView';
 
-export default function MemoEditScreen() {
+export default function MemoEditScreen(props) {
+    const { navigation } = props;
     return (
-        <KeyboardAvoidingView style={styles.container} behavior='height'>
-
-            <AppBar />
+        <KeyboardSafeView style={styles.container}>
 
             <View style={styles.inputContainer}>
                 <TextInput value='買い物リスト' multiline style={styles.input} />
             </View>
-            <CircleButton name='check' />
-        </KeyboardAvoidingView>
+            <CircleButton
+                name='check'
+                onPress={() => { navigation.goBack(); }}
+            />
+        </KeyboardSafeView>
     );
 }
 
